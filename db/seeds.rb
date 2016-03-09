a87d50bced8b26742dfcd84945325e4712b32c77
Torey Hickman <torey@toreyhickman.com>
2016-03-09 12:06:02 -0800

    Seed file with code for reading file

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+Word.delete_all
+
+
+filename = "db/fixtures/abridged_word_list.txt"
+File.foreach(filename) do |line|
+  # write code to create a new record in the database for each line of the input file
+end
